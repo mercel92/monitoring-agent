8b8abe44d3ce252782a3b6e3f0247a5f16b8ea7f
mercel92 <mercelnet@gmail.com>
2017-05-23 15:16:19 +0300

    Update py

--- update.py
+++ update.py
@@ -1,54 +1,41 @@
-import signal,time,json,os,subprocess,platform
+import signal,time,json,os,subprocess,platform, pip
+from bson import json_util
 
 Timeout = 10
 Running = True
 ServiceAddress = 'http://srv.tsoft.club/index/update/'
 TriggerFile = './simple.sh'
-
-
 try:
     import requests
 except ImportError:
-
     pip.main(['install', 'requests'])
     import requests
 
 def executeScript():
-
     global TriggerFile
     print(TriggerFile)
     subprocess.call([TriggerFile])
     print('worked')
 
 def checkService() :
-
     global ServiceAddress
     Hostname = platform.uname()[1]
     print(ServiceAddress+Hostname)
     r = requests.get(ServiceAddress+Hostname)
     try:
-        k = json.loads(r.json())
+        k = json.dumps(r.json(), ensure_ascii=True, default=json_util.default)
         print(k)
-
     except:
         return False
-
-    return False
+    return True
 
 def main():
-
     while Running:
-
         if (checkService() == True):
-            try:
-                executeScript()
-            except:
-                print('Sh is not worked')
+            executeScript()
+            print('Sh is not worked')
         else:
             print('No')
-
         time.sleep(Timeout)
-
 if __name__ == '__main__':
   main()
-
