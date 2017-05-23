3eb3d458355037beb6880639bd2dc66c6e0851a7
mercel92 <mercelnet@gmail.com>
2017-05-23 12:24:11 +0300

    Update py

--- update.py
+++ update.py
@@ -1,6 +1,4 @@
 import signal,time,json,os,subprocess
-from subprocess import Popen, PIPE
-
 
 Timeout = 5
 Running = True
@@ -10,6 +8,7 @@ TriggerFile = './simple.sh'
 try:
     import requests
 except ImportError:
+
     pip.main(['install', 'requests'])
     import requests
 
@@ -20,13 +19,22 @@ def executeScript():
     subprocess.call([TriggerFile])
     print('worked')
 
-def main():
+def checkService() :
+    global ServiceAddress
     r = requests.get(ServiceAddress)
     print(r.status_code)
 
+def main():
+
     while Running:
-        print('test loop\n')
-        executeScript()
+
+        checkService()
+
+        try:
+            executeScript()
+        except:
+            print('Sh is not worked')
+
         time.sleep(Timeout)
 
 if __name__ == '__main__':
