02c841ec790bd8a53aa8e8997f8f3177ce03f71a
mercel92 <mercelnet@gmail.com>
2017-05-23 12:06:22 +0300

    Update py

--- update.py
+++ update.py
@@ -1,9 +1,18 @@
-import signal,time
+import signal,time,json
 
 Timeout = 5
 Running = True
 
+try:
+    import requests
+except ImportError:
+    pip.main(['install', 'requests'])
+    import requests
+
 def main():
+    r = requests.get('http://ns991.tekrom.com:9292/servers')
+    print(r.status_code)
+
     while Running:
         print('test loop')
         time.sleep(Timeout)
