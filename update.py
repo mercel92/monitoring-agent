480d47755ea55c961847c3c1cb3895afbaa14a3e
mercel92 <mercelnet@gmail.com>
2017-05-23 12:13:10 +0300

    Update py

--- update.py
+++ update.py
@@ -1,7 +1,9 @@
-import signal,time,json
+import signal,time,json,os,subprocess
 
 Timeout = 5
 Running = True
+ServiceAddress = 'http://ns991.tekrom.com:9292/servers'
+TriggerFile = ''
 
 try:
     import requests
@@ -9,12 +11,24 @@ except ImportError:
     pip.main(['install', 'requests'])
     import requests
 
+def executeScript():
+
+    shellscript = subprocess.Popen([TriggerFile], stdin=subprocess.PIPE)
+    shellscript.stdin.write("yes is worked\n")
+    shellscript.stdin.close()
+    returncode = shellscript.wait()  # blocks until shellscript is done
+
+    for line in shellscript.stdout.readlines():
+        print(line)
+
+
 def main():
-    r = requests.get('http://ns991.tekrom.com:9292/servers')
+    r = requests.get(ServiceAddress)
     print(r.status_code)
 
     while Running:
-        print('test loop')
+        print('test loop\n')
+        executeScript()
         time.sleep(Timeout)
 
 if __name__ == '__main__':
