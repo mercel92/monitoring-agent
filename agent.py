1421d5e271bb2b520538f3516fdf17611537ada0
mercel92 <mercelnet@gmail.com>
2017-05-24 09:20:32 +0300

    Update py

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #chmod test
+#test#test
 import socket, signal
 import sys, os, platform
 import json, time
--- update.py
+++ update.py
@@ -1,5 +1,5 @@
 import signal,time,json,os,subprocess,platform, pip
-
+##test##test
 Timeout = 60*1
 Running = True
 ServiceAddress = 'http://srv.tsoft.club/index/update/'
